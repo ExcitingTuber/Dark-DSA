0f61b7830c6aede932380b38024aaaf233832c52
Mayank Gautam <138778742+ExcitingTuber@users.noreply.github.com>
2026-05-20 18:24:11 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (98.96%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -13,30 +13,21 @@
 class Solution {
 public:
     int getHeight(TreeNode* node) {
-        // Base case: empty tree has height 0 (or -1 depending on definition)
         if (node == nullptr) {
             return 0;
         }
-
-        // Recursive step: 1 + max height of subtrees
         return 1 + max(getHeight(node->left), getHeight(node->right));
     }
 
     vector<vector<int>> levelOrder(TreeNode* root) {
         int h = getHeight(root);
-        // cout<<h;
         vector<vector<int>> ans(h);
         queue<TreeNode*> q;
         int i = 0, j = 1, k = 0;
         if (!root)
             return {};
         q.push(root);
-        // if (root->left != NULL) {
-        //     k++;
-        // }
-        // if (root->right != NULL) {
-        //     k++;
-        // }
+        
         while (!q.empty()) {
             TreeNode* curr = q.front();
             q.pop();
