d788f59948baf0e223f5434095de203b7e157124
Mayank Gautam <138778742+ExcitingTuber@users.noreply.github.com>
2026-05-24 19:15:38 +0530

    Time: 394 ms (47.33%) | Memory: 185.8 MB (36.86%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -2,29 +2,18 @@ class Solution {
 public:
     void merge(vector<int>& a, int s, int m, int e){
         int n1=m+1-s, n2=e-m;
-        vector<int>L(n1,0);
-        vector<int>R(n2,0);
-        for(int i=0;i<n1;i++){
-            L[i]=a[s+i];
-        }
-        for(int i=0;i<n2;i++){
-            R[i]=a[m+1+i];
-        }
+        vector<int>L(n1,0),R(n2,0);
+
+        for(int i=0;i<n1;i++) L[i]=a[s+i];
+        for(int i=0;i<n2;i++) R[i]=a[m+1+i];
+
         int i=0,j=0,k=s;
         while(i<n1 && j<n2){
-            if(L[i]<R[j]){
-                a[k++]=L[i++];
-            }
-            else{
-                a[k++]=R[j++];
-            }
+            if(L[i]<R[j])a[k++]=L[i++];
+            else a[k++]=R[j++];
         }
-        while(i<n1){
-            a[k++]=L[i++];
-        }
-        while(j<n2){
-            a[k++]=R[j++];
-        }
+        while(i<n1)a[k++]=L[i++];
+        while(i<n2)a[k++]=R[j++];
     }
     void Msort(vector<int>& a, int s, int e){
         if(s<e){
@@ -35,6 +24,14 @@ public:
         }
     }
     vector<int> sortArray(vector<int>& a) {
+        // for(int i=1;i<a.size();i++){
+        //     int k=a[i], j=i-1;
+        //     while(j>=0 && a[j]>k){
+        //         a[j+1]=a[j];
+        //         j--;
+        //     }
+        //     a[j+1]=k;
+        // }
         Msort(a,0,a.size()-1);
         return a;
     }
